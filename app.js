a75e6d887086848a20f87267d4d2a7683267aaf2
Gaju kadtan <gajanankadtan57@gamil.com>
2021-11-24 11:25:46 +0530

    Add submit secret functionality.

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -95,17 +96,50 @@ app.get('/register', function (req, res) {
 });
 
 app.get('/secrets', function (req, res) {  
-    if (req.isAuthenticated()){
-        res.render('secrets');
-    } else {
-        res.redirect('/login');
-    }
-})
+    User.find({"secret": {$ne:null}}, function (err, foundUsers) { 
+      if(err){
+        console.log(err);
+      }else {
+        res.render("secrets", {usersWithSecret: foundUsers});
+
+      }
+     });
+});
+
 
 app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
 });
+
+// Submiting users new secrets on database
+
+
+app.get('/submit', function (req, res) { 
+      if (req.isAuthenticated()){
+        res.render('submit');
+    } else {
+        res.redirect('/login');
+    }
+ });
+
+ app.post('/submit', function (req, res) {
+   const userSecret = req.body.secret;
+
+   console.log(req.user);
+   User.findById(req.user.id, function (err, foundOne) { 
+     if (err){
+       console.log(err);
+     } else {
+       if (foundOne){
+         foundOne.secret = userSecret;
+         foundOne.save(function (err) { 
+           res.redirect('/secrets');
+          });
+       }
+     }
+    });
+   });
 
 app.post('/register', function (req, res) {
     User.register({username: req.body.username}, req.body.password, function (err, user) {  
